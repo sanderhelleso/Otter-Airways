f6a69d596ce54c251aef68982100d183e95c1b65
Sanderhelleso <sanderhelleso@hotmail.com>
2018-12-05 21:57:02 -0800

    added events for main menu buttons

--- src/main/java/flg/flightreservationsystem/MainActivity.java
+++ src/main/java/flg/flightreservationsystem/MainActivity.java
@@ -1,7 +1,15 @@
 package flg.flightreservationsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import flg.flightreservationsystem.activities.CancelReservationActivity;
+import flg.flightreservationsystem.activities.CreateAccountActivity;
+import flg.flightreservationsystem.activities.ManageSystemActivity;
+import flg.flightreservationsystem.activities.ReserveSeatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,81 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize main menu
+        initializeMenu();
+    }
+
+    void initializeMenu() {
+
+        // initialize each of the menu option
+        initializeCreateAccount();
+        initializeReserveSeat();
+        initializeCancelReservation();
+        initializeManageSystem();
+    }
+
+    // initialize create account intent
+    void initializeCreateAccount() {
+
+        // fetch element by id
+        final Button CREATE_ACCOUNT = (Button) findViewById(R.id.create_account);
+        CREATE_ACCOUNT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // open intent on button click
+                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    // initialize reserve seat intent
+    void initializeReserveSeat() {
+
+        // fetch element by id
+        final Button RESERVE_SEAT = (Button) findViewById(R.id.reserve_seat);
+        RESERVE_SEAT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // open intent on button click
+                Intent intent = new Intent(MainActivity.this, ReserveSeatActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    // initialize cancel reservation intent
+    void initializeCancelReservation() {
+
+        // fetch element by id
+        final Button CANCEL_RESERVATION = (Button) findViewById(R.id.cancel_reservation);
+        CANCEL_RESERVATION.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // open intent on button click
+                Intent intent = new Intent(MainActivity.this, CancelReservationActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    // initialize cancel reservation intent
+    void initializeManageSystem() {
+
+        // fetch element by id
+        final Button MANAGE_SYSTEM = (Button) findViewById(R.id.manage_system);
+        MANAGE_SYSTEM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // open intent on button click
+                Intent intent = new Intent(MainActivity.this, ManageSystemActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
---
+++ src/main/java/flg/flightreservationsystem/activities/ReserveSeatActivity.java
@@ -0,0 +1,13 @@
+package flg.flightreservationsystem.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReserveSeatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_seat);
+    }
+}
